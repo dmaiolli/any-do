b9078662b3e0cd6ddc5d4e4142b88c3871b7d765
dmaiolli <denysmaiolli@hotmail.com>
2021-03-28 20:24:03 -0300

    adicionado estilos globais styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #EBEBEB;
+    color: #6A6A6A;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Josefin sans', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
